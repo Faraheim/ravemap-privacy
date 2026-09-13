b1e8a5afca4701798b65993eab15257c6421faf2
Ravemap <bomberomapp@gmail.com>
2026-09-13 20:33:34 +0200

    Publish Ravemap PWA under /pwa.

---
+++ pwa/sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'ravemap-web-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html', './manifest.json'])).then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        if (res.ok && event.request.url.startsWith(self.location.origin)) {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((hit) => hit || caches.match('./index.html'))),
+  );
+});
